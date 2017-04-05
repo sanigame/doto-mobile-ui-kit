e8b75191008245a0609deec97013787824eac9ee
Supasan Suklim <supasan.s@dotography.com>
2017-04-05 10:55:11 +0700

    Copy all file

--- develop/Gruntfile.js
+++ develop/Gruntfile.js
@@ -1,69 +1,23 @@
 module.exports = function (grunt) {
 
-  grunt.loadNpmTasks('grunt-babel')
-  grunt.loadNpmTasks('grunt-contrib-clean')
-  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-copy')
 
   grunt.initConfig({
-    babel: {
-      options: {
-        sourceMap: true
-      },
-      dist: {
-        files: [
-          {
-            '../lib/index.js': 'src/app.js'
-          },
-          {
-            expand: true,
-            cwd: 'src/containers/',
-            src: ['**/*.js'],
-            dest: '../lib/containers/'
-          },
-          {
-            expand: true,
-            cwd: 'src/components/',
-            src: ['**/*.js'],
-            dest: '../lib/components/'
-          },
-          {
-            expand: true,
-            cwd: 'src/themes/',
-            src: ['**/*.js'],
-            dest: '../lib/themes/'
-          }
-        ]
-      }
-    },
-    clean: {
-      folder: ['../lib/**/']
-    },
     copy: {
       main: {
         expand: true,
-        cwd: 'src/assets/',
-        src: '**',
-        dest: '../lib/assets/',
-      },
-    },
-    concat: {
-      options: {
-        sourceMap: true,
-        sourceMapStyle: 'link',
-      },
-      js: {
+        cwd: 'src/',
         src: [
-          'src/app.js',
-          'src/containers/**/*.js',
-          'src/components/**/*.js',
-          'src/themes/**/*.js'
+          'app.js',
+          'containers/**/*.js',
+          'components/**/*.js',
+          'themes/**/*.js',
+          'assets/**/*'
         ],
-        dest: '../lib/script.js'
-      }
+        dest: '../lib/',
+      },
     },
   });
 
-  //grunt.registerTask('default', ['clean', 'babel', 'copy'])
-  grunt.registerTask('default', ['concat'])
+  grunt.registerTask('default', ['copy'])
 };
