8f5a129a9fa33a933edeabad340d3f231a1d431c
Supasan Suklim <supasan.s@dotography.com>
2017-04-05 11:00:21 +0700

    Up to version 0.4.0

--- develop/src/Router.js
+++ develop/src/Router.js
@@ -10,10 +10,10 @@ import {
 const RouterComponent = () => {
   return (
     <Router sceneStyle={{ marginTop: 70 }}>
-      <Scene key="home" component={HomePage} title="Home" />
+      <Scene key="home" component={HomePage} title="Home" initial />
       <Scene key="colors" component={ColorPage} title="Colors" />
       <Scene key="buttons" component={ButtonPage} title="Buttons"  />
-      <Scene key="examples" component={ExamplesPage} title="Examples" initial />
+      <Scene key="examples" component={ExamplesPage} title="Examples" />
     </Router>
   );
 };
